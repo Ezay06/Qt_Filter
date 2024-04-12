fa9da2135fa8841831e7ed579708b09339f997ef
Ziad Omar <61595704+Ezay06@users.noreply.github.com>
2024-04-13 01:34:46 +0200

    version 1.0

--- main.cpp
+++ main.cpp
@@ -11,9 +11,4 @@ int main(int argc, char *argv[])
 }
 
 
-/*issues till now:
- * merge filter is incomplete, still need to develop a window to accept a second image.
- * Skewing filter is half implemented, still need to develop a window to accept user input.
- * behind bars filter is un-implemented, pressing button in menu leads to nothing .
- * wrong naming the behind bar button
-*/
+//Baby Photoshop version 1.0

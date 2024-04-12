9376dc5b5e27a7ded81fe2e77d48996280db1a44
Abdelrahman Mohamed <abdogawad02@gmail.com>
2024-04-12 20:53:41 +0200

    fancy frame added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1130,9 +1130,61 @@ void MainWindow::on_red_frame_button_clicked()
         out_image.saveImage(in_image_path);
         QPixmap pixmap(qin_image_path);
         ui->current_image->setPixmap(pixmap);
-
-
     }
+
+    if(choice = '2'){
+        for(int i=0;i<image.width;++i){
+            for(int j=0;j<image.height;++j){
+
+                //draw the white square bounded the maximum is 60
+                if(((i>image.width-60 && i<image.width-53 || j>53 && j<60) && i>image.width-60 && j<60) ||
+                    ((i>image.width-48 && i<image.width-45 || j>45 && j<48) && i>image.width-48 && j<48) ||(i>image.width-35 && j<35) ||
+                    ((i<60 && i>53 || j<image.height-53 && j>image.height-60) && i<60 && j>image.height-60) ||
+                    ((i<48 && i>45 || j<image.height-45 && j>image.height-48) && i<48 && j>image.height-48) ||(i<35 && j>image.height-35)||
+                    ((i<60 && i>53 || j>53 && j<60) && i<60 && j<60) || ((i<48 && i>45 || j>45 && j<48) && i<48 && j<48) ||(i<35 && j<35)||
+                    ((i>image.width-60 && i<image.width-53 || j<image.height-53 && j>image.height-60) && i>image.width-60 && j>image.height-60) ||
+                    ((i>image.width-48 && i<image.width-45 || j<image.height-45 && j>image.height-48) && i>image.width-48 && j>image.height-48) ||
+                    (i>image.width-35 && j>image.height-35)){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
+                }
+                //draw the colored lines
+                if(i<20 || j<20 || i>image.width-20 || j>image.height-20){
+                    image(i,j,0)=225;
+                    image(i,j,1)=0;
+                    image(i,j,2)=0;
+                }
+                //draw the white straight lines
+                if((i>10 &&i<13       ) || (i>16 && i<20) || (j>10 && j<13) || (j>16 && j<20) || (i<image.width-10 && i>image.width-13) || (i<image.width-16 && i>image.width-20) || (j<image.height-10 && j>image.height-13) || (j<image.height-16 && j>image.height-20) ){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
+                }
+                //This part draws the eight circles each line draws one circle
+                if((square(j-30)+square(i-image.width+60+10)<150 && square(j-30)+square(i-image.width+60+10)>100) ||
+                    /*5*/ (square(i-30)+square(j-image.height+70)<150 && square(i-30)+square(j-image.height+70)>100)||
+                    (square(i-70)+square(j-30)>100 && square(i-70)+square(j-30)<150) ||
+                    (square(j-70)+square(i-30)>100 && square(j-70)+square(i-30)<150) ||
+                    (square(j-70)+square(i-image.width+30)>100 && square(j-70)+square(i-image.width+30)<150) ||
+                    /*6*/(square(i-70)+square(j-image.height+30)>100 && square(i-70)+square(j-image.height+30)<150) ||
+                    (square(i-image.width+30)+square(j-image.height+70)>100 && square(i-image.width+30)+square(j-image.height+70)<150) ||
+                    /*8*/(square(j-image.height+30)+square(i-image.width+70)>100 && (square(j-image.height+30)+square(i-image.width+70)<150))
+                    ){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
+                }
+            }
+        }
+        out_image = image;
+        curr_image = out_image;
+        out_image.saveImage(in_image_path);
+        QPixmap pixmap(qin_image_path);
+        ui->current_image->setPixmap(pixmap);
+    }
+
+
 }
 void MainWindow::on_green_frame_button_clicked()
 {
@@ -1167,9 +1219,57 @@ void MainWindow::on_green_frame_button_clicked()
         out_image.saveImage(in_image_path);
         QPixmap pixmap(qin_image_path);
         ui->current_image->setPixmap(pixmap);
+    }
+    if(choice = '2'){
+        for(int i=0;i<image.width;++i){
+            for(int j=0;j<image.height;++j){
 
-
-
+                //draw the white square bounded the maximum is 60
+                if(((i>image.width-60 && i<image.width-53 || j>53 && j<60) && i>image.width-60 && j<60) ||
+                    ((i>image.width-48 && i<image.width-45 || j>45 && j<48) && i>image.width-48 && j<48) ||(i>image.width-35 && j<35) ||
+                    ((i<60 && i>53 || j<image.height-53 && j>image.height-60) && i<60 && j>image.height-60) ||
+                    ((i<48 && i>45 || j<image.height-45 && j>image.height-48) && i<48 && j>image.height-48) ||(i<35 && j>image.height-35)||
+                    ((i<60 && i>53 || j>53 && j<60) && i<60 && j<60) || ((i<48 && i>45 || j>45 && j<48) && i<48 && j<48) ||(i<35 && j<35)||
+                    ((i>image.width-60 && i<image.width-53 || j<image.height-53 && j>image.height-60) && i>image.width-60 && j>image.height-60) ||
+                    ((i>image.width-48 && i<image.width-45 || j<image.height-45 && j>image.height-48) && i>image.width-48 && j>image.height-48) ||
+                    (i>image.width-35 && j>image.height-35)){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
+                }
+                //draw the colored lines
+                if(i<20 || j<20 || i>image.width-20 || j>image.height-20){
+                    image(i,j,0)=0;
+                    image(i,j,1)=225;
+                    image(i,j,2)=0;
+                }
+                //draw the white straight lines
+                if((i>10 &&i<13       ) || (i>16 && i<20) || (j>10 && j<13) || (j>16 && j<20) || (i<image.width-10 && i>image.width-13) || (i<image.width-16 && i>image.width-20) || (j<image.height-10 && j>image.height-13) || (j<image.height-16 && j>image.height-20) ){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
+                }
+                //This part draws the eight circles each line draws one circle
+                if((square(j-30)+square(i-image.width+60+10)<150 && square(j-30)+square(i-image.width+60+10)>100) ||
+                    /*5*/ (square(i-30)+square(j-image.height+70)<150 && square(i-30)+square(j-image.height+70)>100)||
+                    (square(i-70)+square(j-30)>100 && square(i-70)+square(j-30)<150) ||
+                    (square(j-70)+square(i-30)>100 && square(j-70)+square(i-30)<150) ||
+                    (square(j-70)+square(i-image.width+30)>100 && square(j-70)+square(i-image.width+30)<150) ||
+                    /*6*/(square(i-70)+square(j-image.height+30)>100 && square(i-70)+square(j-image.height+30)<150) ||
+                    (square(i-image.width+30)+square(j-image.height+70)>100 && square(i-image.width+30)+square(j-image.height+70)<150) ||
+                    /*8*/(square(j-image.height+30)+square(i-image.width+70)>100 && (square(j-image.height+30)+square(i-image.width+70)<150))
+                    ){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
+                }
+            }
+        }
+        out_image = image;
+        curr_image = out_image;
+        out_image.saveImage(in_image_path);
+        QPixmap pixmap(qin_image_path);
+        ui->current_image->setPixmap(pixmap);
     }
 }
 void MainWindow::on_blue_frame_button_clicked()
@@ -1197,6 +1297,57 @@ void MainWindow::on_blue_frame_button_clicked()
                     image(i,j,0)=30;
                     image(i,j,1)=30;
                     image(i,j,2)=30;
+                }
+            }
+        }
+        out_image = image;
+        curr_image = out_image;
+        out_image.saveImage(in_image_path);
+        QPixmap pixmap(qin_image_path);
+        ui->current_image->setPixmap(pixmap);
+    }
+    if(choice = '2'){
+        for(int i=0;i<image.width;++i){
+            for(int j=0;j<image.height;++j){
+
+                //draw the white square bounded the maximum is 60
+                if(((i>image.width-60 && i<image.width-53 || j>53 && j<60) && i>image.width-60 && j<60) ||
+                    ((i>image.width-48 && i<image.width-45 || j>45 && j<48) && i>image.width-48 && j<48) ||(i>image.width-35 && j<35) ||
+                    ((i<60 && i>53 || j<image.height-53 && j>image.height-60) && i<60 && j>image.height-60) ||
+                    ((i<48 && i>45 || j<image.height-45 && j>image.height-48) && i<48 && j>image.height-48) ||(i<35 && j>image.height-35)||
+                    ((i<60 && i>53 || j>53 && j<60) && i<60 && j<60) || ((i<48 && i>45 || j>45 && j<48) && i<48 && j<48) ||(i<35 && j<35)||
+                    ((i>image.width-60 && i<image.width-53 || j<image.height-53 && j>image.height-60) && i>image.width-60 && j>image.height-60) ||
+                    ((i>image.width-48 && i<image.width-45 || j<image.height-45 && j>image.height-48) && i>image.width-48 && j>image.height-48) ||
+                    (i>image.width-35 && j>image.height-35)){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
+                }
+                //draw the colored lines
+                if(i<20 || j<20 || i>image.width-20 || j>image.height-20){
+                    image(i,j,0)=0;
+                    image(i,j,1)=0;
+                    image(i,j,2)=225;
+                }
+                //draw the white straight lines
+                if((i>10 &&i<13       ) || (i>16 && i<20) || (j>10 && j<13) || (j>16 && j<20) || (i<image.width-10 && i>image.width-13) || (i<image.width-16 && i>image.width-20) || (j<image.height-10 && j>image.height-13) || (j<image.height-16 && j>image.height-20) ){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
+                }
+                //This part draws the eight circles each line draws one circle
+                if((square(j-30)+square(i-image.width+60+10)<150 && square(j-30)+square(i-image.width+60+10)>100) ||
+                    /*5*/ (square(i-30)+square(j-image.height+70)<150 && square(i-30)+square(j-image.height+70)>100)||
+                    (square(i-70)+square(j-30)>100 && square(i-70)+square(j-30)<150) ||
+                    (square(j-70)+square(i-30)>100 && square(j-70)+square(i-30)<150) ||
+                    (square(j-70)+square(i-image.width+30)>100 && square(j-70)+square(i-image.width+30)<150) ||
+                    /*6*/(square(i-70)+square(j-image.height+30)>100 && square(i-70)+square(j-image.height+30)<150) ||
+                    (square(i-image.width+30)+square(j-image.height+70)>100 && square(i-image.width+30)+square(j-image.height+70)<150) ||
+                    /*8*/(square(j-image.height+30)+square(i-image.width+70)>100 && (square(j-image.height+30)+square(i-image.width+70)<150))
+                    ){
+                    image(i,j,0)=255;
+                    image(i,j,1)=255;
+                    image(i,j,2)=255;
                 }
             }
         }

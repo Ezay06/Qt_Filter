a1df265e2d5b0e09c2829bf985c55c783cfb635b
Abdelrahman Mohamed <abdogawad02@gmail.com>
2024-04-17 04:47:23 +0200

    added a theme to the program

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+
+#include <QFileInfo>
+
+
+QString getMainCppPath()
+{
+    // __FILE__ is a predefined macro that expands to the name of the current source file
+    return QFileInfo(__FILE__).absolutePath();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+
+    QString mainCppPath = getMainCppPath()+"/SyNet.qss";
+
+    //The theme
+    QFile styleSheetFile(mainCppPath);
+    styleSheetFile.open(QFile::ReadOnly);
+
+    QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    a.setStyleSheet(styleSheet);
+
+
     w.show();
     return a.exec();
 }
